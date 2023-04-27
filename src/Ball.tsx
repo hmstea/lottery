f24c11e386f2fcd4a93315f16ab919fd4453ae61
misun hwang <hmstea@gmail.com>
2023-04-27 14:11:23 +0900

    볼사이즈

--- src/Ball.tsx
+++ src/Ball.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 function Ball({color, num}:Props) {
     return (
-        <div key={num} className={`rounded-full md:w-[50px] md:h-[50px] sm:w-[40px] sm:h-[40px] flex items-center justify-center shadow-ms `} style={{backgroundColor: color}}>
+        <div key={num} className={`rounded-full w-[40px] h-[40px] flex items-center justify-center shadow-ms `} style={{backgroundColor: color}}>
             <span className='text-2xl font-bold m-0 p-0'>{num}</span>
         </div>
     );
